e1fb037fc34fcf0ceb3c79044c4f2d9e56b62a69
HashbrownKazang <admin@spuntentertainment.com>
2025-04-08 06:15:26 +0000

    save

--- src/consts/supported_tokens.ts
+++ src/consts/supported_tokens.ts
@@ -1,5 +1,5 @@
 import type { Chain } from "thirdweb";
-import { avalancheFuji, polygonAmoy, sepolia } from "./chains";
+import { polygon } from "./chains";
 
 export type Token = {
   tokenAddress: string;
@@ -22,23 +22,6 @@ export type SupportedTokens = {
  */
 export const SUPPORTED_TOKENS: SupportedTokens[] = [
   {
-    chain: avalancheFuji,
-    tokens: [
-      {
-        tokenAddress: "0x5425890298aed601595a70ab815c96711a31bc65",
-        symbol: "USDC",
-        icon: "/erc20-icons/usdc.png",
-      },
-      {
-        tokenAddress: "0x82dcec6aa3c8bfe2c96d40d8805ee0da15708643",
-        symbol: "USDT",
-        icon: "/erc20-icons/usdt.png",
-      },
-      // Add more ERC20 here...
-    ],
-  },
-
-  {
     chain: polygon,
     tokens: [
       {
@@ -48,22 +31,6 @@ export const SUPPORTED_TOKENS: SupportedTokens[] = [
       },
       {
         tokenAddress: "0xc2132D05D31c914a87C6611C10748AEb04B58e8F",
-        symbol: "USDT",
-        icon: "/erc20-icons/usdt.png",
-      },
-    ],
-  },
-
-  {
-    chain: sepolia,
-    tokens: [
-      {
-        tokenAddress: "0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238",
-        symbol: "USDC",
-        icon: "/erc20-icons/usdc.png",
-      },
-      {
-        tokenAddress: "0x36160274b0ed3673e67f2ca5923560a7a0c523aa",
         symbol: "USDT",
         icon: "/erc20-icons/usdt.png",
       },
