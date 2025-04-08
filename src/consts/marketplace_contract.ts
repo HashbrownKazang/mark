116eb03437f18db3187a3b7974ee54175afb001e
HashbrownKazang <admin@spuntentertainment.com>
2025-04-08 07:12:23 +0000

    save

--- src/consts/marketplace_contract.ts
+++ src/consts/marketplace_contract.ts
@@ -15,4 +15,8 @@ export const MARKETPLACE_CONTRACTS: MarketplaceContract[] = [
     address: "0x19804fFA27AE6aCeA3e895b532D64A003Ce18bd6",
     chain: polygon,
   },
+  {
+    address: "0x88B429Ff63aF913bbEf13a83441aF93Cde510761",
+    chain: polygon,
+  },
 ];
